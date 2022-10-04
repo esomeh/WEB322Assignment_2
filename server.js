eb217a5a92f3353b6e22ccd720570b3d1d16a353
Samuel Omeh <esomeh@myseneca.ca>
2022-10-03 20:29:47 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"/blog.html"));
+});
+
+app.get("/article", function(req,res){
+    res.sendFile(path.join(__dirname,"/read_more.html"));
+  });
+  
+  app.get("/registration", function(req,res){
+    res.sendFile(path.join(__dirname,"/registration.html"));
+  });
+  app.get("/login", function(req,res){
+    res.sendFile(path.join(__dirname,"/login.html"));
+  });
+
+  app.use(express.static("static"));
+
+app.listen(HTTP_PORT, onHttpStart);
+
